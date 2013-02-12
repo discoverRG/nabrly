e16d4d042ea71d722f483fc443844e8c5fb8f6f1
Randy Greene <randy@discoverRG.com>
2013-02-11 18:01:55 -0600

    Initial Commit (Setup)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nabrly::Application.config.secret_token = 'f27aabcc67946c3a7bd818b87ba09ea5df3ad66c9bbb283d842c0ef640b1d262b8a4ed0ead4126f6f13ba0e24030c6dc8f817e3de48c9c309bf311bc64318700'
